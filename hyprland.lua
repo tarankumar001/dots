76e2f8c8771e9e0e82924450869880323e4bff5d
Tarankumar P <YOUR_GITHUB_EMAIL>
2026-07-24 11:32:10 +0530

    dot

---
+++ hyprland.lua
@@ -0,0 +1,35 @@
+require("modules.monitors")
+require("modules.binds")
+require("modules.autostart")
+require("modules.env")
+require("modules.decorations")
+require("modules.layout")
+require("modules.misc")
+require("modules.input")
+require("modules.windowrules")
+
+-- Ref https://wiki.hypr.land/Configuring/Basics/Workspace-Rules/
+-- "Smart gaps" / "No gaps when only"
+-- uncomment all if you wish to use that.
+-- hl.workspace_rule({ workspace = "w[tv1]", gaps_out = 0, gaps_in = 0 })
+-- hl.workspace_rule({ workspace = "f[1]",   gaps_out = 0, gaps_in = 0 })
+-- hl.window_rule({
+--     name  = "no-gaps-wtv1",
+--     match = { float = false, workspace = "w[tv1]" },
+--     border_size = 0,
+--     rounding    = 0,
+-- })
+-- hl.window_rule({
+--     name  = "no-gaps-f1",
+--     match = { float = false, workspace = "f[1]" },
+--     border_size = 0,
+--     rounding    = 0,
+-- })
+
+--
+
+
+
+
+
+

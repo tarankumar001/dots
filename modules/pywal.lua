5c0578b7456260b8418ab93d5f1353742c59fde4
Tarankumar P <YOUR_GITHUB_EMAIL>
2026-07-27 16:56:42 +0530

    fiixed hyperland window pywall

--- modules/pywal.lua
+++ modules/pywal.lua
@@ -1,10 +1,12 @@
-local colors = {}
+local M = {}
 
-for line in io.lines(os.getenv("HOME") .. "/.cache/wal/colors-hyprland.conf") do
+local path = os.getenv("HOME") .. "/.cache/wal/colors-hyprland.conf"
+
+for line in io.lines(path) do
     local key, value = line:match("%$(%w+)%s*=%s*(.+)")
     if key and value then
-        colors[key] = value
+        M[key] = value
     end
 end
 
-return colors
+return M

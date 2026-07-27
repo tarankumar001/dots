f738b3eb9de214c55448b3878325a94520be14bc
Tarankumar P <YOUR_GITHUB_EMAIL>
2026-07-27 16:17:33 +0530

    added clock

--- modules/binds.lua
+++ modules/binds.lua
@@ -5,7 +5,7 @@
 -- Set programs that you use
 local terminal    = "kitty"
 local fileManager = "dolphin"
-local menu        = "hyprlauncher"
+local menu        = "rofi -show drun"
 
 ---------------------
 ---- KEYBINDINGS ----
@@ -20,12 +20,13 @@ local closeWindowBind = hl.bind(mainMod .. " + Q", hl.dsp.window.close())
 hl.bind(mainMod .. " + M", hl.dsp.exec_cmd("command -v hyprshutdown >/dev/null 2>&1 && hyprshutdown || hyprctl dispatch 'hl.dsp.exit()'"))
 hl.bind(mainMod .. " + E", hl.dsp.exec_cmd(fileManager))
 hl.bind(mainMod .. " + V", hl.dsp.window.float({ action = "toggle" }))
-hl.bind(mainMod .. " + D", hl.dsp.exec_cmd("rofi -show drun"))
+hl.bind(mainMod .. " + D", hl.dsp.exec_cmd(menu))
+hl.bind(mainMod .. " + R", hl.dsp.exec_cmd("/home/t10nat/.config/waybar/launch.sh"))
 hl.bind(mainMod .. " + P", hl.dsp.window.pseudo())
 hl.bind(mainMod .. " + J", hl.dsp.layout("togglesplit"))    -- dwindle only
 
 -- Move focus with mainMod + arrow keys
--- hl.bind(mainMod .. " + left",  hl.dsp.focus({ direction = "left" }))
+-- hl.bind(mainMod .. " + left",  hl.dsp.focus({ direction = d"left" }))
 -- hl.bind(mainMod .. " + right", hl.dsp.focus({ direction = "right" }))
 -- hl.bind(mainMod .. " + up",    hl.dsp.focus({ direction = "up" }))
 -- hl.bind(mainMod .. " + down",  hl.dsp.focus({ direction = "down" }))

2becbbd72e8148a952b1c5be1bf38fbdb490aa4c
Tarankumar P <YOUR_GITHUB_EMAIL>
2026-07-27 20:23:31 +0530

    sleep

--- modules/binds.lua
+++ modules/binds.lua
@@ -86,4 +86,5 @@ hl.bind("XF86AudioNext",  hl.dsp.exec_cmd("playerctl next"),       { locked = tr
 hl.bind("XF86AudioPause", hl.dsp.exec_cmd("playerctl play-pause"), { locked = true })
 hl.bind("XF86AudioPlay",  hl.dsp.exec_cmd("playerctl play-pause"), { locked = true })
 hl.bind("XF86AudioPrev",  hl.dsp.exec_cmd("playerctl previous"),   { locked = true })
+hl.bind("SUPER + SHIFT + S", hl.dsp.exec_cmd("sleep 1 && systemctl suspend"))
 
